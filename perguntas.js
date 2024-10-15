7cb94e50d61071c1791821d8d3e1e6d2c6e8c14a
Maria Clara Nogueira <148371027+Nogueira2208@users.noreply.github.com>
2024-10-15 15:49:09 -0300

    oi

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Língua inglesa',
+    'Como se diz oi em Inglềs?',
+    'Oi em Inglês é HI (RAI)'
+)
+
+criaCartao(
+    'Biologia',
+    'Quem é considerado o pai da genética?',
+    'Gregor Mendel'
+)
+
+criaCartao(
+    'Mtemática',
+    'Qual é o valor de Pi?',
+    '3,14'
+)

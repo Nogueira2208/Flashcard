eabb6a550c63e866cdb89862b1f7f36bb593fd09
Maria Clara Nogueira <148371027+Nogueira2208@users.noreply.github.com>
2024-11-05 16:04:05 -0300

    3333

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
-)
-
-criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Biologia',
+    'O que é um alelo?',
+    'Os alelos são responsáveis por determinr uma mesma caracteŕistca, como a cor dos olhos.'
 )
 
 criaCartao(
     'Biologia',
-    'Quem é considerado o pai da genética?',
-    'Gregor Mendel'
+    'Qual é a função do DNA?',
+    'A função do DNA é armazenar e transmitir informações genéticas de um organismo.'
 )
 
 criaCartao(
-    'Matemática',
-    'Qual é o valor de Pi?',
-    '3,14'
+    'Biologia',
+    'O que significa ser homozigoto?',
+    ' ser homozigoto significa um organismo que possui dois alelos idênticos para uma determinada característica.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a diferença entre herança dominante e recessiva?',
+    'A diferença é a quantidade de cópias de um gene necessárias para expressar um traço'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é mutação genética?',
+    'Mutação genética é uma alteração permanente na sequência de nucleotídes do DNA.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é um cromossomo',
+    'Um cromossomo é uma estrutura que contêm o material genético de uma célula, sendo formados por uma molécula de DNA e proteínas.'
 )
